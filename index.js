8ec7c3c7181c445ce283d72479baf58d267d6618
Masis <Masis23@gmail.com>
2022-10-30 22:16:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,93 @@
 //global variables
 var number;
 var number2;
+var number3;
 var getMax;
 var getMin;
 
-// JavaScript
-// Wrap the native DOM audio element play function and handle any autoplay errors
-Audio.prototype.play = (function(play) {
-return function () {
-  var audio = this,
-      args = arguments,
-      promise = play.apply(audio, args);
-  if (promise !== undefined) {
-    promise.catch(_ => {
-      // Autoplay was prevented. This is optional, but add a button to start playing.
-      var el = document.createElement("button");  //class="btn btn-primary"
-      el.class = "btn btn primary";
-	  el.innerHTML = "Play";
-      el.addEventListener("click", function(){play.apply(audio, args);});
-      this.parentNode.insertBefore(el, this.nextSibling)
-    });
-  }
-};
-})(Audio.prototype.play);
 
-// Try automatically playing our audio via script. This would normally trigger and error.
-document.getElementById('MyAudioElement').muted = false; 
-document.getElementById('MyAudioElement').play()
+var scoreHuman = document.getElementById("humanScore"); //get score from html id value
+//getRandomNumbers();
+//var remove = document.getElementById("p").remove();
+//document.getElementById('getInput').remove(); //NOT NEEDED keep for placeholder
+//window.setTimeout(function(){location.reload()},7000); //NOT NEEDED keep for placeholder
+//document.getElementById("button1").addEventListener('click', randomNumbers); //NOT NEEDED keep for placeholder
 
-/*
+//function startGame() { //initial start game	
+	getRandomNumbers();
+	getUserInput();
+	//document.getElementById("startbtn").remove();
+	//document.querySelector("p").remove();
+//}
+
+function getRandomNumbers() {
+  
+  number=document.getElementById("random1").innerHTML = Math.floor(Math.random() * 20);
+  number2=document.getElementById("random2").innerHTML = Math.floor(Math.random() * 20);
+  number3=document.getElementById("random3").innerHTML = Math.floor(Math.random() * 20);
+	if(number === 0 || number2 === 0 || number3 === 0) {
+		getRandomNumbers();
+	}
+  getMax = Math.max(number, number2, number3);
+  getMin = Math.min(number, number2, number3);
+  //getEqual = Math.equal(number, number2, number3);
+  console.log(getMax);
+  console.log(getMin);
+  
+	//check if all numbers are equal first
+	//	if(getEqual) {
+		//	Tie();
+		//}
+	if(number === number2 && number === number3 || number2 === number3 && number === number3) { // 2 2 2
+				Tie();
+	}  
+  
+}
+
+
+ function getUserInput() {
+	 
+	  //get user input
+  var secondInput = document.getElementById("getInput").value; //Get number from text input
+		//console.log(secondInput);
+			if(getMax === +secondInput) {
+				scoreResultsWIN();
+			//document.querySelector("p").remove();				
+				//document.querySelector("p").innerHTML = "";
+			}
+				
+			
+			else if(getMin === +secondInput) { scoreResultsLOOSE(); }
+
+
+
+ } //end function
+  
+
+ function scoreResultsWIN() {
+	//keep within same page!!!!
+	document.querySelector("p").innerHTML = '<p style="color:green">Correct!</p>' + getMax + " is the greater number! "; //change the html text
+	scoreHuman.innerHTML = parseInt(scoreHuman.innerHTML)+1;
+	getRandomNumbers();	
+	
+	
+	//document.getElementById("p").remove();
+ } 
+ 
+ function scoreResultsLOOSE() {
+	//keep within same page!!!!
+	document.querySelector("p").innerHTML = '<p style="color:red">WRONG!</p>' + getMin + " is the smaller number! "; //change the html text
+	scoreHuman.innerHTML = parseInt(scoreHuman.innerHTML)-1;
+	getRandomNumbers();
+ }
+ 
+ function Tie() {
+	document.querySelector("p").innerHTML = '<p style="color:blue">TIE!</p>'; //change the html text
+	getRandomNumbers();
+ }
+
+//document.querySelector("p").remove();
+
  function play_pause() {
         var myAudio = document.getElementById("myAudio");
         if (myAudio.paused) {
@@ -37,51 +96,3 @@ document.getElementById('MyAudioElement').play()
             myAudio.pause();
         }
     }
-*/
-
-
-
-function randomNumbers() {
-  number=document.getElementById("demo").innerHTML = Math.floor(Math.random() * 20);
-  number2=document.getElementById("demo3").innerHTML = Math.floor(Math.random() * 20);
-	
-	if(number === number2) {
-				document.getElementById('demo').remove();
-				document.getElementById('button1').remove();
-				document.getElementById('button2').remove();
-				document.getElementById('demo1').remove();
-				document.getElementById('which').remove();
-				document.querySelector("p").innerHTML = '<p style="color:blue">TIE!</p>' + number + " = " + number2; //change the html text
-				window.setTimeout(function(){location.reload()},7000);
-	}
-  
-  //get maximum number
-  getMax = Math.max(number, number2);
-  getMin = Math.min(number, number2);
-  console.log(getMax);  //16
-}
-     
-document.getElementById("button1").addEventListener('click', randomNumbers)
-document.getElementById("button2").addEventListener('click', game)
-
-function game() {
-	var secondInput = document.getElementById("demo1").value; //Get number from text input
-			if(getMax === +secondInput) {
-				document.getElementById('demo').remove();
-				document.getElementById('button1').remove();
-				document.getElementById('button2').remove();
-				document.getElementById('demo1').remove();
-				document.getElementById('which').remove();
-				document.querySelector("p").innerHTML = getMax + " is greater than " + getMin; //change the html text
-				window.setTimeout(function(){location.reload()},7000);
-			}
-			else {
-				document.getElementById('demo').remove();
-				document.getElementById('button1').remove();
-				document.getElementById('button2').remove();
-				document.getElementById('demo1').remove();
-				document.getElementById('which').remove();
-				document.querySelector("p").innerHTML = getMin + " is smaller than " + getMax; //change the html text
-				window.setTimeout(function(){location.reload()},7000);
-  }
-} //end function
